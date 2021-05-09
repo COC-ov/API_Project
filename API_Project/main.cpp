febcbf2f0cfb31d13fd6ceba032e15276ca6864f
cuteDazai <cutedazai@gmail.com>
2021-05-10 01:25:58 +0900

    비트맵 이미지 출력하기

--- API_Project/main.cpp
+++ API_Project/main.cpp
@@ -1,7 +1,9 @@
 #include <windows.h>
-#include<TCHAR.H>
+#include <TCHAR.H>
+#include "resource.h"
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE hInst;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpszCmdLine, int nCmdShow)
@@ -11,6 +13,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	WNDCLASS wndClass;
 	TCHAR className[11] = L"Class Name";
 	TCHAR titleName[10] = L"다자이 귀여워";
+	hInst = hInstance;
 
 	wndClass.style = CS_HREDRAW | CS_VREDRAW;
 	wndClass.lpfnWndProc = WndProc;
@@ -38,8 +41,25 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	HDC hDC, memDC;
+	PAINTSTRUCT ps;
+	static HBITMAP hBitmap;
+
 	switch (uMsg)
 	{
+	case WM_CREATE:
+		hBitmap = (HBITMAP)LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP1));	//처음엔 안되었다가 껐다가키니까됨, 비트맵 이미지를 로드
+		break;																//hInst는 응용프로그램의 인스턴스 값
+
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		memDC = CreateCompatibleDC(hDC);	//메모리디시인memedc생성
+		SelectObject(memDC, hBitmap);		//memdc에 hBitmap을 서정
+		BitBlt(hDC, 0, 0, 463, 492, memDC, 0, 0, SRCCOPY);	//그림을 0,0부터 너비463픽셀, 높이492픽셀 만큼 복사해 좌표 0,0부터 그리라는 의미
+		DeleteDC(memDC);									//memDC삭제
+		EndPaint(hWnd, &ps);
+		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
